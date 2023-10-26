4754dd3ca8b1911ab8f6125205b713e63e68003d
xhgua <369034959@qq.com>
2023-10-26 16:56:52 +0800

    Site updated: 2023-10-26 16:56:52

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@
     }
 
     function createFoldButton(fold) {
-        return '<span class="fold">' + (fold === 'unfolded' ? '<i class="fas fa-angle-down"></i>' : '<i class="fas fa-angle-right"></i>') + ' Code</span>';
+        return '<span class="fold">' + (fold === 'unfolded' ? '<i class="fas fa-angle-down"></i>' : '<i class="fas fa-angle-right"></i>') + '</span>';
     }
 
     $('figure.highlight table').wrap('<div class="highlight-body">');
